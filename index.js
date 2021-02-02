c2150c482a833ab6770d63772794084bfbd6b2ff
Ahmed Abdelsalam <ahmedabdelsalam82.de@gmail.com>
2021-02-02 14:46:14 +0100

    Done

--- index.js
+++ index.js
@@ -1,17 +1,65 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    let added = false;
+    this.length += 1;
+    for(let itemIndex in this.items){
+      if(item <= this.items[itemIndex]) {
+        this.items.splice(itemIndex, 0, item);
+        added = true
+        break
+      }
+    }
+    if(added === false) {
+        this.items.push(item);
+    }
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(pos < this.length)
+    {
+      return this.items[pos];
+    } else {
+      throw new Error('OutOfBounds');
+    }
+    
+  }
 
-  max() {}
+  max() {
+    if(this.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items.reduce((x, y) => (x > y) ? x : y)
+    }
+  }
 
-  min() {}
+  min() {
+    if(this.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items.reduce((x, y) => (x > y) ? y : x)
+    }
+  }
 
-  sum() {}
+  sum() {
+    if(this.length === 0) {
+      return 0;
+    } else {
+      return this.items.reduce((x, y) => x + y);
+    }
+  }
 
-  avg() {}
+  avg() {
+    if(this.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.sum()/this.length;
+    }
+  }
 }
 
 module.exports = SortedList;
